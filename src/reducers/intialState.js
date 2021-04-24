8c2bee600ce85a871c63b8baafd611adf441c9d1
adam.bayraktar <adam.bayraktar@grantsolutions.gov>
2021-04-24 11:39:00 -0400

    updated state

--- src/reducers/intialState.js
+++ src/reducers/intialState.js
@@ -4,7 +4,7 @@ const initState = {
       {id: '2', title: 'Charmander Laid an Egg', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat'},
       {id: '3', title: 'a Helix Fossil was Found', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat'},
       {id: '4', title: 'Abdurrahman Bayraktar', body: 'Lorem ipsum, dolor sit ameg elit. Consequatur voluptate laborum perferendis, Abdurrahman'},
-      {id: '5', title: 'Cigdem Esen Bayraktar', body: 'Lorem ipsum, dolor sit ameg elit. Consequatur voluptate laborum perferendis, Cigdem Esen Bayraktar'} 
+      {id: '5', title: 'Ekrem Bayraktar', body: 'Lorem ipsum, dolor sit ameg elit. Consequatur voluptate laborum perferendis, Ekrem Bayraktar'} 
     ]
   }
 
